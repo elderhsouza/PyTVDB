f19d3ac63120e6ccf684d0f679991d52a7130c87
Elder Henrique Souza <elderhenrique@gmail.com>
2014-05-02 23:09:03 -0300

    data caching added

--- py-tvdb.py
+++ py-tvdb.py
@@ -4,15 +4,35 @@ from zipfile import ZipFile
 from tempfile import TemporaryFile
 from bs4 import BeautifulSoup
 
-class PyTVDB(object):
-	#TODO implement data cache for subsequent queries
-	mirror   = 'http://thetvdb.com'
 
-	def __init__(self, api_key, language='en'):
+'''
+# ------------------- TODO
+
+- output options (object or xml)
+
+'''
+
+class PyTVDB(object) :
+	#TODO implement data cache for subsequent queries
+	mirror = 'http://thetvdb.com'
+
+	cache_id   = None
+	data_cache = {
+		'series': None
+	}
+
+	def __init__(self, api_key, language='en') :
 		self.api_key  = api_key
 		self.language = language
 
-	def search(self, query):
+	def _check_cache(self, series_id) :
+		return str(self.cache_id) == str(series_id)
+
+	def _flush_cache(self) :
+		self.cache_id = None
+		self.data_cache['series'] = None
+
+	def search(self, query) :
 		'search thetvdb.com database for shows containing the query'
 
 		response = requests.get('%s/api/GetSeries.php?seriesname=%s' % (self.mirror, query))
@@ -20,12 +40,10 @@ class PyTVDB(object):
 
 		data = BeautifulSoup(response.text).find_all('series')
 
-		if len(data) < 1:
-			return
-		else:
+		if len(data) > 1 :
 			results = []
 
-			for series in data:
+			for series in data :
 				node_value = lambda node : node.string if node != None else None
 
 				results.append({
@@ -38,23 +56,40 @@ class PyTVDB(object):
 					'imdb_id'		: node_value(series.imdb_id),
 					'zap2it_id'		: node_value(series.zap2it_id)
 				})
+		else :
+			return
+
 		return results
 
-	def get_series(self, series_id):
+	def get_series(self, series_id) :
 		'get series info'
 
-		response = requests.get('%s/api/%s/series/%s/all/%s.zip' % (self.mirror, self.api_key, series_id, self.language))
-		response.raise_for_status()
+		# TODO raise TypeError | maybe not
+		# TODO refactor data request logic
+		
+		if self._check_cache(series_id) :
+			data = self.data_cache['series']
 
-		with TemporaryFile() as tmp_file :
-			tmp_file.write(response.content)
-			with ZipFile(tmp_file, 'r') as zip_file :
-				with zip_file.open('%s.xml' % (self.language)) as xml_file :
-					data    = BeautifulSoup(xml_file.read())
-					series  = data.find('series')
-					seasons = max([ int(s.string) for s in data.find_all('seasonnumber') ])
-					# TODO maybe replace bs for lxml for css selectors like :last-child ?
+		else :
+			response = requests.get('%s/api/%s/series/%s/all/%s.zip' % (self.mirror, self.api_key, series_id, self.language))
+			
+			if response.status_code != 200 :
+				return
 
+			self._flush_cache()
+
+			with TemporaryFile() as tmp_file :
+				tmp_file.write(response.content)
+				with ZipFile(tmp_file, 'r') as zip_file :
+					# TODO e
+					with zip_file.open('%s.xml' % (self.language)) as xml_file :
+						data = BeautifulSoup(xml_file.read())
+
+		series  = data.find('series')
+		seasons = max([ int(s.string) for s in data.find_all('seasonnumber') ])
+		# TODO maybe replace bs for lxml for css selectors like :last-child ?
+
+		# TODO prepend image urls with the appropriate domain path
 		result = {
 			'tvdb_id'			: series.id.string,
 			'title'				: series.seriesname.string,
@@ -81,60 +116,74 @@ class PyTVDB(object):
 			'poster'			: series.poster.string,
 			'seasons'			: seasons
 		}
+		self.cache_id = series_id
+		self.data_cache['series'] = data
+
 		return result
 
-	def get_all_episodes(self, series_id):
+	def get_all_episodes(self, series_id) :
 		pass
 
-	def get_episodes_by_season(self, series_id, season_number):
+	def get_episodes_by_season(self, series_id, season_number) :
 		pass
 
-	def get_episode(self, episode_id):
+	def get_episode(self, episode_id) :
 		pass
 
-	def get_actors(self, series_id):
+	def get_actors(self, series_id) :
 		pass
 
 	# server_time 1399036356
-	def get_updates(self, server_time, *series_id):
+	def get_updates(self, server_time, *series_id) :
 		pass
 
 	#TODO implement graphics request
 
-	def get_graphics(self, series_id):
+	def get_graphics(self, series_id) :
 		pass
 
-	def get_banners(self, series_id):
+	def get_banners(self, series_id) :
 		pass
 
-	def get_posters(self, series_id):
+	def get_posters(self, series_id) :
 		pass
 
-	def get_fanart(self, series_id):
+	def get_fanart(self, series_id) :
 		pass
 
-	def get_series_posters(self, series_id):
+	def get_series_posters(self, series_id) :
 		pass
 
-	def get_season_posters(self, series_id, season_number):
+	def get_season_posters(self, series_id, season_number) :
 		pass
-
-	## #TODO data caching disposal routine - maybe?
-
-	def __del__(self):
-		print 'destroy pytvdb'
 		
 
 #------------ TESTING
 
-
 tvdb   = PyTVDB('D229EEECFE78BA5F')
 
-# search = tvdb.search('thrones')
-# print search
+search = tvdb.search('thrones')
+print search
 # series = search[1]
 # print series
 
 # info = tvdb.get_series(series['tvdb_id'])
-info = tvdb.get_series(121361) # 121361 game of thrones | 81189 breaking bad
+# 121361 game of thrones | 81189 breaking bad
+
+# info = tvdb.get_series(121361) # 121361 game of thrones
+# print info
+
+# info = tvdb.get_series(121361) # 121361 game of thrones
+# print info
+
+# info = tvdb.get_series(81189) # 81189 breaking bad
+# print info
+
+# info = tvdb.get_series(81189) # 81189 breaking bad
+# print info
+
+info = tvdb.get_series(121361) # 121361 game of thrones
+print info
+
+info = tvdb.get_series(9999999999999999) # mal-formed id
 print info
